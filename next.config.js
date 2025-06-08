202752b5210e39e1194e7893af96587e6a33775d
keijuyanagawa <kyanagawa999@gmail.com>
2025-06-08 17:25:54 +0900

    next.config.js修正

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
-require('dotenv').config()
+// require('dotenv').config()
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     env: {
+//         // Reference a variable that was defined in the .env file and make it available at Build Time
+//         API_ENDPOINT: process.env.API_ENDPOINT,
+//       },
+// }
+
+// module.exports = nextConfig
+
+require('dotenv').config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
-      },
-}
+  // 追加: standalone ビルドを有効化
+  output: 'standalone',
 
-module.exports = nextConfig
+  // 既存の環境変数設定はそのまま
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  },
+};
+
+module.exports = nextConfig;
